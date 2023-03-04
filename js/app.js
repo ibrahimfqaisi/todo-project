01da74e57be5e70df25aee49b39c39085788ddb8
ibrahimfqaisi <qasrawi.ibrahim1998@gmail.com>
2023-03-04 21:41:08 +0000

    add new three (Yes/No) questions.

--- js/app.js
+++ js/app.js
@@ -1,36 +1,64 @@
 'use strict';
 
 console.log("hh");
-var userName;
-var userGender;
-var userAge;
-var theAnsswer;
-userName = prompt("enter your name");
-userGender = prompt("enter your gender ");
+let userName;
+let userGender;
+let userAge;
+let theAnsswer;
+let likeSport;
+let hungry;
+let busy;
+let yourInformation=[];
+userName ="invalid"
+
+userName ="enter your name: "+ prompt("enter your name");
+yourInformation.push(userName);
+
+userGender ="enter your gender: "+ prompt("enter your gender ");
 
 
-
-while (userGender !== 'male' && userGender != "female") {
-    console.log('testing')
-
+while (userGender !== "enter your gender: "+'male' && userGender !="enter your gender: "+"female") {
+    
     alert("The answer should be either (male or female)");
-    userGender = prompt("enter your gender ");
+    userGender ="enter your gender: "+ prompt("enter your gender");
 };
+yourInformation.push(userGender);
 
 
-userAge = prompt("enter your age");
+userAge = "enter your age: "+ prompt("enter your age");
 while (userAge <= 0) {
     alert("your userAge is wrong inter it again");
     userAge = prompt("enter your age");
 }
+yourInformation.push(userAge);
 
-theAnsswer = confirm("Do you wants to skip the welcoming message?");
-console.log(theAnsswer);
-if (theAnsswer == false) {
-    if (userGender == 'male') {
+likeSport = 'do you like sport ? '+ confirm('do you like sport ?');
+
+console.log( likeSport);
+yourInformation.push(likeSport);
+ 
+hungry = 'are you hungry ? ' + confirm('are you hungry ?');
+yourInformation.push(hungry);
+console.log(hungry);
+
+busy = 'are you busy ? ' +confirm( 'are you busy ?');
+console.log( busy) 
+yourInformation.push(busy);
+
+
+theAnsswer ="Do you wants to skip the welcoming message? "+ confirm("Do you wants to skip the welcoming message?");
+console.log( theAnsswer);
+if (theAnsswer == "Do you wants to skip the welcoming message? "+false) {
+    if (userGender == "enter your gender: "+'male') {
         alert('welcom Mr ' + userName);
     }
     else {
         alert('welcom Ms ' + userName);
     };
 };
+yourInformation.push(theAnsswer);
+console.log(yourInformation);
+console.log(yourInformation.length);
+for(let i =0 ; i<yourInformation.length;i++){
+    console.log(yourInformation[i]) ;
+}
