40cd00d116b123fc5e47f6f7f449e0b7af72caca
ibrahimfqaisi <qasrawi.ibrahim1998@gmail.com>
2023-03-05 08:48:49 +0000

    i made my code better

--- js/app.js
+++ js/app.js
@@ -8,57 +8,85 @@ let theAnsswer;
 let likeSport;
 let hungry;
 let busy;
-let yourInformation=[];
-userName ="invalid"
-
-userName ="enter your name: "+ prompt("enter your name");
-yourInformation.push(userName);
-
-userGender ="enter your gender: "+ prompt("enter your gender ");
-
-
-while (userGender !== "enter your gender: "+'male' && userGender !="enter your gender: "+"female") {
-    
-    alert("The answer should be either (male or female)");
-    userGender ="enter your gender: "+ prompt("enter your gender");
+let yourInformation = [];
+let question1;
+let question2;
+let question3;
+let question4;
+let question5;
+let question6;
+let question7;
+let elemnt;
+function upload(elemnt) {
+    yourInformation.push(elemnt);
 };
-yourInformation.push(userGender);
+
+userName = prompt("enter your name");
+upload(userName);
+question1 = "enter your name :";
+upload(question1);
+
+userGender = prompt("enter your gender ");
+while (userGender !== 'male' && userGender !== "female") {
+
+    alert("The answer should be either (male or female)");
+    userGender = prompt("enter your gender");
+};
+upload(userGender);
+question2 = "enter your gender :";
+upload(question2);
 
 
-userAge = "enter your age: "+ prompt("enter your age");
+userAge = prompt("enter your age");
 while (userAge <= 0) {
     alert("your userAge is wrong inter it again");
     userAge = prompt("enter your age");
 }
-yourInformation.push(userAge);
-
-likeSport = 'do you like sport ? '+ confirm('do you like sport ?');
-
-console.log( likeSport);
-yourInformation.push(likeSport);
- 
-hungry = 'are you hungry ? ' + confirm('are you hungry ?');
-yourInformation.push(hungry);
-console.log(hungry);
-
-busy = 'are you busy ? ' +confirm( 'are you busy ?');
-console.log( busy) 
-yourInformation.push(busy);
+upload(userAge);
+question3 = "enter your age :";
+upload(question3);
 
 
-theAnsswer ="Do you wants to skip the welcoming message? "+ confirm("Do you wants to skip the welcoming message?");
-console.log( theAnsswer);
-if (theAnsswer == "Do you wants to skip the welcoming message? "+false) {
-    if (userGender == "enter your gender: "+'male') {
+
+likeSport = prompt('do you like sport ?');
+upload(likeSport);
+question4 = 'do you like sport ?';
+upload(question4);
+
+
+
+hungry = prompt('are you hungry ?');
+upload(hungry);
+question5 = 'are you hungry ?';
+upload(question5);
+
+
+busy = prompt('are you busy ?');
+upload(busy);
+question6 = 'are you busy ?';
+upload(question6);
+
+theAnsswer = confirm("Do you wants to skip the welcoming message? ");
+if (theAnsswer == false) {
+    if (userGender == 'male') {
         alert('welcom Mr ' + userName);
     }
     else {
         alert('welcom Ms ' + userName);
     };
 };
-yourInformation.push(theAnsswer);
+upload(theAnsswer);
+question7 = "Do you wants to skip the welcoming message? ";
+upload(question7);
+
 console.log(yourInformation);
+
 console.log(yourInformation.length);
-for(let i =0 ; i<yourInformation.length;i++){
-    console.log(yourInformation[i]) ;
-}
+for (let i = 0; i < yourInformation.length; i += 2) {
+    if (yourInformation[i] == '') {
+        console.log((yourInformation[i + 1]) + ' invalid');
+    } else {
+        console.log((yourInformation[i + 1]) + yourInformation[i]);
+    }
+
+};
